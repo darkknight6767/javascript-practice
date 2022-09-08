512287dd8332b764bbffb42d97bc242cdcc67a31
darkknight6767 <78910295+darkknight6767@users.noreply.github.com>
2022-09-08 23:26:21 +0530

    Add files via upload

---
+++ listFromAnArray.js
@@ -0,0 +1,11 @@
+let languages =["kotlin","java","ada","python","Ruby"];
+let result=[];
+
+let createListItem=(value)=>{
+    let element="<li>"+value+"</li>";
+    result.push(element);
+};
+//languages.forEach(createListItem);
+createListItem("kotlin");
+let html=result.join(" ");
+document.getElementById("lang").innerHTML=html;
